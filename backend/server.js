c4372d74c719e0e947b7c30982310479dd853245
justinlees <166937890+varunHemanth45@users.noreply.github.com>
2025-05-06 15:07:53 +0530

    C103

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 /* third party middleware */
 
-//cors setup configuration
+//cors setup settings
 
 app.options("*", (req, res) => {
   // res.header(
